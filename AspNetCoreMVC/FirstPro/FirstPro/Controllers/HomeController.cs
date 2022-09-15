2e9223ae8adf54ccfd0cb2deb5501f4af89bdbda
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-09-15 18:24:52 +0300

    Loyout Ve RendeBody Yapısı öğrenirdi.
    Temel konular IAction model gönderme vs yapıldı.

---
+++ AspNetCoreMVC/FirstPro/FirstPro/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstPro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstPro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public int Index3()
+        {
+            return 34;
+        }
+        public string Index4()
+        {
+            return "Merhaba Bugün Hava Ortalama Derecede";
+        }
+        public IActionResult Index5()
+        {
+            var Kitaplar = new List<Kitap>()
+            {
+                new Kitap{ID=1,KitapAd="Kuyucaklı Yusuf",Yazar="Sebattin Ali"},
+                new Kitap{ID=2,KitapAd="80 Günde Deveri Alem",Yazar="Jules Verne"},
+                new Kitap{ID=3,KitapAd="Bilinmeyen bir kadının mektubu",Yazar="Zweig"}
+            };
+            return View(Kitaplar);
+        }
+
+        public IActionResult Index6()
+        {
+            ViewBag.Deger1 = "Merhaba ViewBag";
+            return View();
+        }
+
+        public IActionResult Index7()
+        {
+            return View();
+        }
+
+        public IActionResult Index8()
+        {
+            return View();
+        }
+
+        public IActionResult Index9()
+        {
+            return View();
+        }
+    }
+}
