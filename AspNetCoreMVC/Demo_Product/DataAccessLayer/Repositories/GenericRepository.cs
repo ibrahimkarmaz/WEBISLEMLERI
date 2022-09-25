3e507f23ea241401ed13478179f2eb071926d603
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-09-25 23:07:35 +0300

    N-Tier Katmanli Mimari (OOP DERSLERİ)
    Katmanların Oluşturulması
    Katmanlara gerekli paketlerin eklenmesi
    Katmanlar arasında referanslar
    EntitiyLayer 'da  Concrete(Somut) klasör oluşturuldu ve entity tanmılandı.
    
    DataAccessLayer'da Concrete(Somut) klasör oluşturuldu ve Context  tanımlandı.
    Context veritabanına aktarıldı
    
    Abstract Klasörü oluşturuldu ve klasör içinde entity işlemlerine ait interfaceler tanımlandı.
    
    Tek çatı altında özellik olarak Generic İnterface birleştirildi ve diğer interfacelere referans verildi.
    
    Reposities Klasörü açıldı ve içine Generic Repository eklendi bu IGenericReposery implame etti.
    
    EntitiyFramer klasörü oluşturuldu.
    Bu Klasörde ise GenericRepository ait işlemlerin yanında entity ait IgenericReposty eklendi.

---
+++ AspNetCoreMVC/Demo_Product/DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var _context = new Context();
+            _context.Remove(t);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            using var _context = new Context();
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            using var _context = new Context();
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            using var _context = new Context();
+            _context.Add(t);
+            _context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var _context = new Context();
+            _context.Update(t);
+        }
+    }
+}
