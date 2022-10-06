496c3073388490696df1e93af32e0f9d8706901e
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-10-06 05:34:26 +0300

    AspNetCoreMvcDevamEdiyor
    İstatistikler penceresi sona erdi.

--- AspNetCoreMVC/CoreAndFood/CoreAndFood/Controllers/ChartController.cs
+++ AspNetCoreMVC/CoreAndFood/CoreAndFood/Controllers/ChartController.cs
@@ -40,7 +40,38 @@ namespace CoreAndFood.Controllers
         }
 
         public IActionResult Statictics()
-        {
+        { Context context = new Context();
+            var deger1 = context.Foods.Count();
+            ViewBag.d1 = deger1;
+
+            var deger2 = context.Categorys.Count();
+            ViewBag.d2 = deger2;
+
+            var deger3 = context.Foods.Where(x=>x.CategoryID==(context.Categorys.Where(a=>a.CategoryName=="Fruit").First().CategoryID)).Count();
+            ViewBag.d3 = deger3;
+
+            var deger4 = context.Foods.Count(x=>x.CategoryID== (context.Categorys.Where(a => a.CategoryName == "Vegetables").First().CategoryID));
+            ViewBag.d4 = deger4;
+
+            var deger5 = context.Foods.Sum(x=>x.Stock);
+            ViewBag.d5 = deger5;
+
+            var deger6 = context.Foods.Count(x => x.CategoryID == (context.Categorys.Where(a => a.CategoryName.ToLower() == "Lagumes".ToLower()).First().CategoryID));
+            ViewBag.d6 = deger6;
+
+            var deger7 = context.Foods.OrderByDescending(x => x.Stock).Select(y => y.Name).FirstOrDefault();
+            ViewBag.d7 = deger7;
+
+            var deger8 = context.Foods.OrderBy(x => x.Stock).Select(y => y.Name).FirstOrDefault();
+            ViewBag.d8 = deger8;
+
+            var deger9 = context.Foods.Average(x => x.Price).ToString("0.00");
+            ViewBag.d9 = deger9;
+
+
+
+
+
             return View();
         }
     }
