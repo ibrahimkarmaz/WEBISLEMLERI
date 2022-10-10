f049692ff867dbcd84a09b9854ec8808611a5508
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-10-10 22:46:35 +0300

    MY Blog Sitesi (Asp.Net.Core5)
    Yapılanlar;
    Katmanlar
    Paketler
    Entitiyler
    Data bağlantıları vs
    Manager Başlangıç ..

---
+++ AspNetCoreMVC/CoreBlog/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EFCategoryRepository _efCategoryRepository;
+
+        public CategoryManager()
+        {
+            _efCategoryRepository = new EFCategoryRepository();
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _efCategoryRepository.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _efCategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _efCategoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return _efCategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _efCategoryRepository.GetListAll();
+        }
+    }
+}
