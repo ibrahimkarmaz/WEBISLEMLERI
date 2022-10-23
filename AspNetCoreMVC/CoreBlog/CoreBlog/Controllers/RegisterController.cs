b0a943af088be3c39be5b4be27e34060a4d8348f
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-10-24 02:32:59 +0300

    Üye İşlemleri
    .

---
+++ AspNetCoreMVC/CoreBlog/CoreBlog/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            //34'den devam et.
+        }
+    }
+}
