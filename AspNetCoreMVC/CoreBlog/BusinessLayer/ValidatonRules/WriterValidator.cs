6b5d7d1ddb7983458183385b05d51f31e72158c7
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-10-24 19:31:38 +0300

    Sweet Alert Kullanımı
    .

---
+++ AspNetCoreMVC/CoreBlog/BusinessLayer/ValidatonRules/WriterValidator.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidatonRules
+{
+	public class WriterValidator:AbstractValidator<Writer>
+	{
+		public WriterValidator()
+		{
+			RuleFor(x => x.WriterName).NotEmpty().WithMessage("YAZAR ADI SOYADI KISMI BOŞ GEÇİLEMEZ...")
+				.MinimumLength(2).WithMessage("EN AZ 2 KARAKTER OLMALI")
+				.MaximumLength(50).WithMessage("EN FAZLA 50 KARAKTER OLMALI");
+			RuleFor(x => x.WriterMail).NotEmpty().WithMessage("MAİL ADRESİ BOŞ GEÇİLEMEZ...");
+			RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("PAROLA BOŞ GEÇİLEMEZ...")
+                .MinimumLength(8).WithMessage("PAROLA 8 KARAKTERDEN KÜÇÜK OLAMAZ...")
+                .MaximumLength(16).WithMessage("PAROLA 16 KARAKTERDEN BÜYÜK OLAMAZ...")
+                .Matches(@"[A-Z]+").WithMessage("Şifrede en az bir büyük harf olmalıdır.")
+                .Matches(@"[a-z]+").WithMessage("Şifrede en az bir küçük harf olmalıdır.")
+                .Matches(@"[0-9]+").WithMessage("Şifrede en az bir rakam olmalıdır");
+        }
+	}
+}
--- AspNetCoreMVC/CoreBlog/CoreBlog/Controllers/RegisterController.cs
+++ AspNetCoreMVC/CoreBlog/CoreBlog/Controllers/RegisterController.cs
@@ -1,10 +1,15 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidatonRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.Controllers
 {
     public class RegisterController : Controller
     {
+        WriterManager _writerManager = new WriterManager(new EFWriterRepository());
         [HttpGet]
         public IActionResult Index()
         {
@@ -13,7 +18,23 @@ namespace CoreBlog.Controllers
         [HttpPost]
         public IActionResult Index(Writer writer)
         {
-            //34'den devam et.
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult result = writerValidator.Validate(writer);
+            if (result.IsValid)
+            {
+                writer.WriteStatus = true;
+                writer.WriterAbout = "Deneme Test";
+                _writerManager.WriterAdd(writer);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View(writer);//EĞER HATA VAR İSE YİNE AYNI PENCERE AÇILIR FAKAT WRİTER ALINAN BİLGİYİ GERİ DÖNDÜRÜR.
         }
     }
 }
