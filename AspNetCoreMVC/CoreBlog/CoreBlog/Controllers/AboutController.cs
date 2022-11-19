eb2671f44d0573b18f2aad7b1adf0481022eb638
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-11-19 23:51:47 +0300

    About Sayfası tamamlandı.(Static Olarak.)
    .

---
+++ AspNetCoreMVC/CoreBlog/CoreBlog/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.Controllers
+{
+	public class AboutController : Controller
+	{//42.VİDEODAN DEVAM ET.
+		AboutManager _aboutManager = new AboutManager(new EFAboutRepository());
+		public IActionResult Index()
+		{
+			var values = _aboutManager.GetList();
+			return View(values);
+		}
+		public PartialViewResult SocialMediaAbout()
+		{
+			return PartialView();
+		}
+	}
+}
