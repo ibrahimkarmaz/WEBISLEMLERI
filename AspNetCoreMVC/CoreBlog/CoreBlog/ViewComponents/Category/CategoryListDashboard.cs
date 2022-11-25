61303994aad7aaa233708635f9af09e0e13c81ef
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-11-25 22:13:15 +0300

    Dashbord işlemleri.
    .

---
+++ AspNetCoreMVC/CoreBlog/CoreBlog/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.ViewComponents.Category
+{
+    public class CategoryListDashboard:ViewComponent
+    {
+        CategoryManager  _categoryManager = new CategoryManager(new EFCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values=_categoryManager.GetList();
+            return View(values);
+        }
+    }
+}
