389ea52da9db2d77587912784c1c80f08f1222ff
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-12-30 23:43:54 +0300

    AspCore5 APİ İŞLEMLERİ (LİSTLEME EKLEME)1

---
+++ AspNetCoreMVC/CoreBlog/BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BlogApiDemo.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DefaultController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult EmployeeeList()
+		{
+			using var _context = new Context();
+			var values = _context.Employees.ToList();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult EmployeeeAdd(Employee employee)
+		{
+			using var _context = new Context();
+			_context.Add(employee);
+			_context.SaveChanges();
+			return Ok();
+		}
+	}
+}
