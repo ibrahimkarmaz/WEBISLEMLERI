b58ba33a57457fca19f1681471666e4e8a41ad00
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2023-09-24 09:24:12 +0300

    Temel OOP Mantığı anlatıldı bazi uygulamalar yapıldı.
    .

---
+++ Proje_OPP/Proje_OPP/Context/DbContextOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proje_OPP.Entity;
+
+namespace Proje_OPP.Context
+{
+	public class DbContextOptions:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=.\\SQLEXPRESS;database=DbNewOopCore;integrated security=true;");
+		}
+
+		public DbSet<Product> products { get; set; }
+		public DbSet<Customer> customers { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
